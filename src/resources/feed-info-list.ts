941ff3eb5b29414f72b43060241324facbb1a3a3
Michio Inoue <790354+minoue-xx@users.noreply.github.com>
2023-07-20 05:53:36 +0900

    add: ホームメードガービッジ

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -39,6 +39,7 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['ProAlgorithm','https://pro-algorithm.com/feed/'],
   ['MATLAB - techBrowse','https://tech-browse.com/tag/matlab/feed/'],
   ['MATLAB - controlabo','https://controlabo.com/category/matlab/feed/'],
+  ['MATLAB - ホームメードガービッジ 家族４人のおもしろてづくりブログ','https://homemadegarbage.com/tag/matlab/feed/'],
 ]);
 
 /**
