5cce36de30197a313348cc975f6aa1e0ac83b88a
Michio Inoue <790354+minoue-xx@users.noreply.github.com>
2026-03-10 11:01:37 +0900

    Change lastModifiedBlogsDate to use dynamic import
    Refactored to use dynamic import for feed data.

--- src/site/_data/lastModifiedBlogsDate.js
+++ src/site/_data/lastModifiedBlogsDate.js
@@ -1,8 +1,14 @@
-const fs = require('fs/promises');
-const path = require('path');
-
-module.exports = async () => {
-  const feedData = JSON.parse(await fs.readFile(path.join(__dirname, '../feeds/feed.json')));
+export default async () => {
+  const feedDataModule = await import('../feeds/feed.json');
+  const feedData = feedDataModule.default;
 
   return new Date(feedData.items[0].date_published).toISOString();
 };
+//const fs = require('fs/promises');
+//const path = require('path');
+
+//module.exports = async () => {
+//  const feedData = JSON.parse(await fs.readFile(path.join(__dirname, '../feeds/feed.json')));
+//
+//  return new Date(feedData.items[0].date_published).toISOString();
+//};
