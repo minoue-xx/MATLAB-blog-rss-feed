834e6645d6e8d3d88e6420045da3545772f091b0
Michio Inoue <790354+minoue-xx@users.noreply.github.com>
2026-03-10 12:35:30 +0900

    Refactor lastModifiedBlogsDate.js for better file handling
    Refactor lastModifiedBlogsDate.js to use fs/promises for reading JSON file and added debug log.

--- src/site/_data/lastModifiedBlogsDate.js
+++ src/site/_data/lastModifiedBlogsDate.js
@@ -1,14 +1,8 @@
-export default async () => {
-  const feedDataModule = await import('../feeds/feed.json');
-  const feedData = feedDataModule.default;
+const fs = require('fs/promises');
+const path = require('path');
 
+module.exports = async () => {
+  const feedData = JSON.parse(await fs.readFile(path.join(__dirname, '../feeds/feed.json')));
+  console.log('デバッグログ:', data); // これを追加
   return new Date(feedData.items[0].date_published).toISOString();
 };
-//const fs = require('fs/promises');
-//const path = require('path');
-
-//module.exports = async () => {
-//  const feedData = JSON.parse(await fs.readFile(path.join(__dirname, '../feeds/feed.json')));
-//
-//  return new Date(feedData.items[0].date_published).toISOString();
-//};
